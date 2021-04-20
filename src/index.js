7d96fe266f6433af218596e783f1826aed6b3b33
Travis Courtney <travis@angry-fly.com>
2021-04-20 19:52:34 -0400

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+
+
+  const form = document.getElementById('create-task-form');
+
+  form.addEventListener('submit', function (e) {
+    e.preventDefault()
+    const taskRequest = document.getElementById('new-task-description')
+    const userInput = taskRequest.value;
+    const ul = document.getElementById('tasks')
+    const li = document.createElement('LI')
+    li.innerHTML = userInput;
+    ul.appendChild(li)
+    taskRequest.value = ""
+    const deleteBtn = document.createElement("BUTTON")
+    deleteBtn.innerText = "x"
+    deleteBtn.className = "delete-btn"
+    li.appendChild(deleteBtn)
+    deleteBtn.addEventListener("click", function(e) {
+      li.remove()
+    })
+
+  })
+
+
+
 });
